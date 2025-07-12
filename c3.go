630d4e977e9099b69168432071ba49fc53ea0e08
xiaoxinmiao <xiaoxm_168@163.com>
2025-07-12 20:52:18 +0800

    v3

--- c3.go
+++ c3.go
@@ -3,7 +3,3 @@ package gomod3C
 func AddC(a, b int) int {
 	return a + b
 }
-
-func MultiC(a, b int) int {
-	return a * b
-}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/relax-space/gomod3C/v2
+module github.com/relax-space/gomod3C/v3
 
 go 1.24.4
